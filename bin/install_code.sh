445821070291a36649b6aac3b19d407771ce120d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-06-13 22:25:10 +0000

    install script run

--- bin/install_code.sh
+++ bin/install_code.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo cp -r millegrilles_senseurspassifs senseurspassifs_rpi /var/opt/millegrilles/python
+sudo cp -rv bin/start_senseurspassifs_hub.sh /var/opt/millegrilles/bin/start_senseurspassifs_hub.sh
+
+sudo cp -rv millegrilles_senseurspassifs senseurspassifs_rpi /var/opt/millegrilles/python
 sudo chown -R mginstance:millegrilles /var/opt/millegrilles/python/millegrilles_senseurspassifs
 sudo chown -R mginstance:millegrilles /var/opt/millegrilles/python/senseurspassifs_rpi
 
