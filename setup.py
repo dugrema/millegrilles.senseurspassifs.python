a10734549c0f8da8af3e1b91fc3ae16327274157
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-12-03 15:21:19 -0500

    Added setup.py to load packages

--- setup.py
+++ setup.py
@@ -5,20 +5,12 @@ __VERSION__ = environ.get('VBUILD') or '2024.0.0'
 
 
 setup(
-    name='millegrilles_web_python',
+    name='millegrilles_senseurspassifs_relay',
     version=__VERSION__,
     packages=find_packages(),
     url='https://github.com/dugrema/millegrilles.web.python',
     license='AFFERO',
     author='Mathieu Dugre',
     author_email='mathieu.dugre@mdugre.info',
-    description='Base web pour les applictions MilleGrilles avec client',
-    install_requires=[
-        'pytz>=2020.4',
-        'aiohttp>=3.8.1,<4',
-        'requests>=2.28.1,<3',
-        'pyjwt',
-        'python-socketio>=5.9,<6',
-        'aiohttp-session==2.12.0'
-    ]
+    description='Web relay for MilleGrilles devices'
 )
